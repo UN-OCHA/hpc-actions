039b31212976ecf90dd41a4f6e461beacc484c85
Pl217 <pl_217@live.com>
2024-04-25 17:29:52 +0200

    Change import style for 'path' library
    In `eslint-plugin-unicorn`, they define 4 import styles:
    - `unassigned` - `import 'foo'` or `require('foo')`
    - `default` - `import path from 'path'` or `const path = require('path')`
    - `namespace` - `import * as path from 'path'` or `const path = require('path')`
    - `named` - `import {inspect} from 'util'` or `const {inspect} = require('util')`
    
    Since updating to a newer version, its `import-style`
    rule has changed and default options per module are:
    - `util` - `named` only
    - `path` - `default` only
    - `chalk` - `default` only
    
    Therefore, we need to change import style for 'path' library

--- action/test/specs/action.spec.ts
+++ action/test/specs/action.spec.ts
@@ -1,7 +1,7 @@
 import git from 'isomorphic-git';
 import * as child_process from 'node:child_process';
 import * as fs from 'node:fs';
-import * as path from 'node:path';
+import path from 'node:path';
 import { promisify } from 'node:util';
 
 import * as util from '../util';
--- action/test/specs/docker.spec.ts
+++ action/test/specs/docker.spec.ts
@@ -1,5 +1,5 @@
 import { promises as fs } from 'node:fs';
-import * as path from 'node:path';
+import path from 'node:path';
 
 import * as util from '../util';
 
