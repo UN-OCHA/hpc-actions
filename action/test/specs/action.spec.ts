52c950fb58a2767957e445eec8b350632ee645b7
Pl217 <pl_217@live.com>
2024-04-25 18:58:45 +0200

    Test building multiple images

--- action/test/specs/action.spec.ts
+++ action/test/specs/action.spec.ts
@@ -934,6 +934,111 @@ describe('action', () => {
                 ],
               ]);
             });
+
+            it('Multiple non-existant images', async () => {
+              const upstream = await util.createTmpDir();
+              const dir = await util.createTmpDir();
+              // Prepare upstream repository
+              await git.init({ fs, dir: upstream });
+              await fs.promises.writeFile(
+                path.join(upstream, 'package.json'),
+                JSON.stringify({
+                  version: '1.2.0',
+                })
+              );
+              await git.add({ fs, dir: upstream, filepath: 'package.json' });
+              await setAuthor(upstream);
+              await exec('git commit -m package', { cwd: upstream });
+              await git.branch({ fs, dir: upstream, ref: `env/${env}` });
+              // Clone into repo we'll run in, and create appropriate branch
+              await exec(`git clone --branch env/${env} ${upstream} ${dir}`);
+              // Run action
+              const config: Config = {
+                ...DEFAULT_CONFIG,
+                dockerImages: [
+                  {
+                    ...DEFAULT_CONFIG.dockerImages[0],
+                    appName: 'unit-test-1',
+                    repository: 'hpc-actions/unit-test-1',
+                  },
+                  {
+                    ...DEFAULT_CONFIG.dockerImages[0],
+                    appName: 'unit-test-2',
+                    repository: 'hpc-actions/unit-test-2',
+                  },
+                ],
+              };
+              await fs.promises.writeFile(CONFIG_FILE, JSON.stringify(config));
+              await fs.promises.writeFile(
+                EVENT_FILE,
+                JSON.stringify({
+                  ref: `refs/heads/env/${env}`,
+                })
+              );
+              const logger = util.newLogger();
+              // Prepare docker mock
+              const pullImage = jest.fn().mockResolvedValue(null);
+              const getMetadata = jest.fn().mockResolvedValue(null);
+              const runBuild = jest.fn().mockResolvedValue(null);
+              const retagImage = jest.fn().mockResolvedValue(null);
+              const pushImage = jest.fn().mockResolvedValue(null);
+              await action
+                .runAction({
+                  env: DEFAULT_ENV,
+                  dir,
+                  logger,
+                  dockerInit: () => ({
+                    login: () => Promise.resolve(),
+                    pullImage,
+                    getMetadata,
+                    runBuild,
+                    retagImage,
+                    pushImage,
+                  }),
+                  gitHubInit: testCompleteGitHubInit,
+                })
+                .then(() => {
+                  throw new Error('Expected error to be thrown');
+                })
+                .catch((error) => expect(error).toBe(testCompleteError));
+              expect(logger.log.mock.calls).toMatchSnapshot();
+              expect({
+                pullImage: pullImage.mock.calls.map((call) => [call[0]]),
+                getMetadata: getMetadata.mock.calls,
+                retagImage: retagImage.mock.calls,
+                pushImage: pushImage.mock.calls,
+              }).toMatchSnapshot();
+              const sha = await git.resolveRef({ fs, dir, ref: 'HEAD' });
+              const head = await git.readCommit({ fs, dir, oid: sha });
+              const args1: DockerImageBuildArgs = {
+                commitSha: head.oid,
+                treeSha: head.commit.tree,
+                appToBuild: 'unit-test-1',
+              };
+              const args2: DockerImageBuildArgs = {
+                commitSha: head.oid,
+                treeSha: head.commit.tree,
+                appToBuild: 'unit-test-2',
+              };
+              expect(runBuild.mock.calls).toEqual([
+                [
+                  {
+                    cwd: dir,
+                    logger,
+                    tag: env === 'prod' ? 'v1.2.0' : 'v1.2.0-pre',
+                    args: args1,
+                  },
+                ],
+                [
+                  {
+                    cwd: dir,
+                    logger,
+                    tag: env === 'prod' ? 'v1.2.0' : 'v1.2.0-pre',
+                    args: args2,
+                  },
+                ],
+              ]);
+            });
           });
 
           describe('deployments + mergeback', () => {
